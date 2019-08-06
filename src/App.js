9c2268e0f99175628b37c3fbf0abc100297c53fd
thecodehubdev <thecodehubdev@gmail.com>
2019-08-06 22:56:32 +0300

    added the oauth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import graph from "fb-react-sdk";
 
 function App() {
+  const getAccesToken = () => {
+    var authUrl = graph.getOauthUrl({
+      client_id: "2326480507388664",
+      redirect_uri: "http//localhost:3000"
+    });
+
+    window.open(authUrl, "_blank");
+    console.log(authUrl);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getAccesToken}>Click to open auth</button>
     </div>
   );
 }
